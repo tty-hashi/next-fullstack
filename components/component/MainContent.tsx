a929b0a68e77007d0a78829e13a76f2bc11878f1
tty-hashi <hashi.ndig@gmail.com>
2025-01-13 08:40:14 +0900

    投稿のスクロールを調整

--- components/component/MainContent.tsx
+++ components/component/MainContent.tsx
@@ -1,11 +1,13 @@
-import PostForm from "./PostForm";
-import PostList from "./PostList";
+import PostForm from './PostForm'
+import PostList from './PostList'
 
 export default function MainContent() {
   return (
-    <div className="bg-white dark:bg-gray-800 rounded-lg shadow-md p-4 space-y-6">
+    <div className='bg-white dark:bg-gray-800 rounded-lg shadow-md p-4 space-y-6 flex flex-col h-full overflow-hidden'>
       <PostForm />
-      <PostList />
+      <div className='overflow-y-auto'>
+        <PostList />
+      </div>
     </div>
-  );
+  )
 }
